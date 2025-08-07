e4dbc64b44e431db1cbf344725ee48450b6efcb5
Sanjay-mobrilz <sanjay@mobrilz.agency>
2025-08-07 18:00:09 +0530

    dark theme issue  fixed

--- component/home/Teams.js
+++ component/home/Teams.js
@@ -44,6 +44,7 @@ function Teams() {
     const allLogos = [...teamLogos, ...teamLogos];
 
     return (
+        <div className='bg-white dark:bg-gray-900 py-8'>
         <div className="max-w-7xl mx-auto overflow-hidden py-10 md:py-20">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-6 md:mb-24">
                 <h2 className="text-center text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-gray-100">
@@ -122,6 +123,7 @@ function Teams() {
                     }
                 }
             `}</style>
+        </div>
         </div>
     );
 }
