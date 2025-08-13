e6e012f5baa1a3c2d4439b2ea9b1cd66efc582a8
Devmobrilz <devbrat@mobrilz.agency>
2025-08-13 12:25:16 +0530

    netlify configured

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,35 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//     images: {
+//     domains: ['www.digitalyzeit.com'],
+//     // You can also use remotePatterns for more specific control
+//     // remotePatterns: [
+//     //   {
+//     //     protocol: 'https',
+//     //     hostname: 'www.digitalyzeit.com',
+//     //     pathname: '/wp-content/uploads/**',
+//     //   },
+//     // ],
+//   },
+// };
+
+// export default nextConfig;
+
+
+// Netlify  configuration
+
+
+// next.config.js
+
 /** @type {import('next').NextConfig} */
+const { withNetlify } = require('@netlify/next');
+
 const nextConfig = {
   reactStrictMode: true,
-    images: {
+  images: {
     domains: ['www.digitalyzeit.com'],
-    // You can also use remotePatterns for more specific control
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'www.digitalyzeit.com',
-    //     pathname: '/wp-content/uploads/**',
-    //   },
-    // ],
   },
 };
 
-export default nextConfig;
+export default withNetlify(nextConfig);
