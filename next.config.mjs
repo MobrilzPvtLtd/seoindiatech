614ee51da39afe6ad00b6fe8089d2cbdea4dbb23
Devmobrilz <devbrat@mobrilz.agency>
2025-08-13 12:39:07 +0530

    minor_chnages

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//     images: {
-//     domains: ['www.digitalyzeit.com'],
-//     // You can also use remotePatterns for more specific control
-//     // remotePatterns: [
-//     //   {
-//     //     protocol: 'https',
-//     //     hostname: 'www.digitalyzeit.com',
-//     //     pathname: '/wp-content/uploads/**',
-//     //   },
-//     // ],
-//   },
-// };
-
-// export default nextConfig;
-
-
-// Netlify  configuration
-
-
-// next.config.js
-
 /** @type {import('next').NextConfig} */
-const { withNetlify } = require('@netlify/next');
-
 const nextConfig = {
   reactStrictMode: true,
-  images: {
+    images: {
     domains: ['www.digitalyzeit.com'],
   },
 };
 
-export default withNetlify(nextConfig);
+export default nextConfig;
