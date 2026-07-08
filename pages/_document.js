17ececaf75b83bcbdee2ac744833c6a29ad22309
Harsh162 <harsh@mobrilz.agency>
2026-07-08 10:55:55 +0530

    Added Google Tag Manager integration

--- pages/_document.js
+++ pages/_document.js
@@ -1,44 +1,68 @@
 // pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <meta name="google-site-verification" content="Ag4ojngx0blbdZD0WIjtjNtjFBI_DBJpUAO_di30yWY" />
-        {/* <meta
-            name="google-site-verification"
-            content="fsDpDPzF-AHExzFBGUcxlEITP5MjkpG_ih4WF0w4Y0c"
-          /> */}
-          <meta
-            name="p:domain_verify"
-            content="0c89be4ab18144828edff9618257a5a3"
-          />
-          <meta name="author" content="SEO INDIA TECH" />
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-W00JV5XW2Y"
-          ></script>
-          {/* moved inline gtag config to body to avoid blocking head */}
-        
-            
-       </Head>
-      <body>
-        <Main />
-        <NextScript />
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-W00JV5XW2Y');
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5TS8QJRM');
             `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        <meta
+          name="google-site-verification"
+          content="Ag4ojngx0blbdZD0WIjtjNtjFBI_DBJpUAO_di30yWY"
+        />
+
+        <meta
+          name="p:domain_verify"
+          content="0c89be4ab18144828edff9618257a5a3"
+        />
+
+        <meta name="author" content="SEO INDIA TECH" />
+
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-W00JV5XW2Y"
+        />
+      </Head>
+
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5TS8QJRM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        <Main />
+        <NextScript />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-W00JV5XW2Y');
+          `,
           }}
         />
       </body>
     </Html>
   );
 }
-
- 
