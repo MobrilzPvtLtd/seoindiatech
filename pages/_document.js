817cf9033314c8a60b8afd6ba4942bcfb9fc73d9
imarvindo <sales@mobrilz.com>
2026-08-13 22:26:59 +0530

    Use build:netlify on deploy to publish SVG logo fix; add build marker meta.

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
   return (
     <Html lang="en" suppressHydrationWarning>
       <Head>
+        <meta name="sit-build" content="svg-logo-2026-08-13" />
         <link rel="icon" href="/images/brand/favicon.png" type="image/png" />
         <meta name="author" content="SEO INDIA TECH" />
         <meta
